c9e56bc3d8717e9b7a4f783148adf70a847b42a0
MegamiTenMyles <MylesMatthews2112@Gmail.com>
2021-11-10 15:20:12 +0000

    question answered

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 //write your code here
+function stripUpperCase (string){
+    let newstring = ""
+    let letter
+    for (let i = 0; i < string.length; i++){
+        letter = string.charAt(i)
+        if (letter === letter.toLowerCase()){
+            newstring = newstring + letter
+        }
+    }
+    return newstring
+}
+console.log(stripUpperCase("This is a String"))
